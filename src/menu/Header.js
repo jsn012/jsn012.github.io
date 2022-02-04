799927e21bedee2940b436a95310bbea4b1e0c1e
maynut <libes1234@naver.com>
2022-02-04 15:58:09 +0900

    ++

--- src/menu/Header.js
+++ src/menu/Header.js
@@ -13,9 +13,9 @@ export default function Header() {
           <Link to='/' />
         </div>
         <button className="header-navBtn" type='button'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-list" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-          </svg>
+          <div className='header-navBtn-border'></div>
+          <div className='header-navBtn-border'></div>
+          <div className='header-navBtn-border'></div>
         </button>
         <div className='header-inner'></div>
         <div className='header-bg'></div>
